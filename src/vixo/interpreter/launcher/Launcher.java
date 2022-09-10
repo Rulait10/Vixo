dd6efeb4b7387549502a453517303dc2ad1b77ab
Rulait10 <rulait10@gmail.com>
2022-09-10 15:05:51 +0300

    Update directory base

---
+++ src/vixo/interpreter/launcher/Launcher.java
@@ -0,0 +1,21 @@
+package vixo.interpreter.launcher;
+
+public class Launcher {
+	
+	private String code;
+	private 	String rezult;
+	
+	public String interpret(String code){
+ 	this.code=code;
+		String[] blocks = code.split("{|}");
+		
+		for(String s:blocks){
+		 
+			
+			
+		}
+		rezult= "0";
+		return rezult;
+	}
+	
+}
